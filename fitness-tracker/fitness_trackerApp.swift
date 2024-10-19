e2b439774fc974b2576543853be50cb638ca1b37
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2024-10-19 23:50:32 +0700

    Initial Commit

---
+++ fitness-tracker/fitness_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_trackerApp.swift
+//  fitness-tracker
+//
+//  Created by Đình Khoa Nguyễn on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct fitness_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
